45131b5ca47b9dae67df9f6e525248760ed6e925
konarkcher <konarkcher@yandex.ru>
2017-05-06 20:30:37 +0300

    added eng_locale.py

---
+++ eng_locale.py
@@ -0,0 +1,34 @@
+# Messages
+
+bot_move = "Bot move: "
+bot_moves = "Bot moves: "
+
+use_start = "Use /start to start the game firstly"
+use_finish = "Use /finish to finish the game firstly"
+choose_color_first = "Use have to choose the color firstly"
+choose_color = "Choose the color:"
+wrong_command = "Wrong command!"
+hello_message = "Show me what you can!\nChoose checker:"
+finished = "Finished!"
+no_games = "You already don't have any games("
+
+help_message = '''
+Need some help?
+
+Use /start to start a game
+Choose checker and a place to put it with the buttons
+To finish the game prematurely, use /finish
+
+Have fun!
+                    '''
+
+replies = ('You lose :(', 'Wrong target cell!', 'Wrong checker!',
+           'Choose checker:', 'Choose target cell:', 'You win!',
+           'Choose target cell again!'
+           )
+
+# Buttons
+
+change_checkr = "Change checker"  # TODO: fix this
+white = "White"
+black = "Black"
